36ce60450f8ac3d7194a97919f7ec32611ecfd94
YhzDetermined <2775458593@qq.com>
2026-03-17 18:03:50 +0800

    新增变异体信息统计

---
+++ src/entity/MutExtInfo.py
@@ -0,0 +1,14 @@
+class MutExtInfo:
+    def __init__(self,layer_id,proportion):
+        self.layer_id = layer_id
+        self.proportion = proportion
+
+    def getLayerId(self):
+        return self.layer_id
+
+    def getProportion(self):
+        return self.proportion
+
+
+
+
